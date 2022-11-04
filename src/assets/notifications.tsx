4ef59ba44d305e8b4b896dd629b51d52f85c0819
Ivan <ivan.hdk@gmail.com>
2022-11-04 08:48:03 +0100

    aws deploy

--- src/assets/notifications.tsx
+++ src/assets/notifications.tsx
@@ -57,15 +57,13 @@ export const notify = (type: string, itemName: string, promise: Promise<void>) =
       },
       error: {
         render({ data }) {
-          if (data) {
-            return (
-              <>
-                <Typography variant="body1" fontWeight={600} display="block">{text.error}</Typography>
-                <Typography variant="body2" display="block">"{itemName}"</Typography>
-                <Typography variant="caption" display="block">{data.message}</Typography>
-              </>
-            )
-          }
+          return (
+            <>
+              <Typography variant="body1" fontWeight={600} display="block">{text.error}</Typography>
+              <Typography variant="body2" display="block">"{itemName}"</Typography>
+              {data !== "undefined" && <Typography variant="caption" display="block">{data.message}</Typography>}
+            </>
+          )
         },
       }
     }
